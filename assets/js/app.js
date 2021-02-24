e82bc2ce63d24972f48864fcbb48244349aea3e1
Jérémy <jeremy.desanta07@gmail.com>
2021-02-24 22:04:22 +0100

    first commit of this page

---
+++ assets/js/app.js
@@ -0,0 +1,58 @@
+const nav = document.querySelector('.nav');
+const backTop = document.querySelector('#top');
+let height = backTop.clientHeight;
+
+window.addEventListener('scroll', () => {
+    if(window.scrollY > height){
+        nav.classList.add('scroll');
+    } else{
+        nav.classList.remove('scroll');
+    }
+})
+
+// navigation 
+	// recuperation des variables
+    const burger = document.querySelector('#burger-nav'); //burger qui contient h1 + les deux span
+    const spancheck = document.querySelector('.span-burger');
+    const spancheck1 = document.querySelector('.span-burger1');
+    const ul = document.querySelector('#ul'); // ul qui se déplace de haut en bas
+
+    var linkNav = document.querySelector('.link-nav');
+    var linkNav1 = document.querySelector('.link-nav1');
+    var linkNav2 = document.querySelector('.link-nav2');
+    var linkNav3 = document.querySelector('.link-nav3');
+    var linkNav4 = document.querySelector('.link-nav4');
+    
+    // si on clic sur le menu burger
+        // on ajoute les class
+        burger.onclick = function(){
+            ul.classList.toggle('translate');
+            spancheck.classList.toggle('rotate1');
+            spancheck1.classList.toggle('rotate2');
+        };
+    // clic sur les  liens qui permettent de scroll + fermer la nav
+        linkNav.onclick = function(){
+            ul.classList.toggle('translate');
+            spancheck.classList.toggle('rotate1');
+            spancheck1.classList.toggle('rotate2');
+        };
+        linkNav1.onclick = function(){
+            ul.classList.toggle('translate');
+            spancheck.classList.toggle('rotate1');
+            spancheck1.classList.toggle('rotate2');
+        };
+        linkNav2.onclick = function(){
+            ul.classList.toggle('translate');
+            spancheck.classList.toggle('rotate1');
+            spancheck1.classList.toggle('rotate2');
+        };
+        linkNav3.onclick = function(){
+            ul.classList.toggle('translate');
+            spancheck.classList.toggle('rotate1');
+            spancheck1.classList.toggle('rotate2');
+        };
+        linkNav4.onclick = function(){
+            ul.classList.toggle('translate');
+            spancheck.classList.toggle('rotate1');
+            spancheck1.classList.toggle('rotate2');
+        };
