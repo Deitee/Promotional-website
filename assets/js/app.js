352ca34f199b013dda865a02ba8f94b8d86b9cb7
De Santa Jérémy <jeremy.desanta07@gmail.com>
2021-04-08 16:35:00 +0200

    Code improvement

--- assets/js/app.js
+++ assets/js/app.js
@@ -20,45 +20,20 @@ document.addEventListener("DOMContentLoaded", function() {
     const spancheck1 = document.querySelector('.span-burger1');
     const ul = document.querySelector('#ul'); // ul qui se déplace de haut en bas
 
-    var linkNav = document.querySelector('.link-nav');
-    var linkNav1 = document.querySelector('.link-nav1');
-    var linkNav2 = document.querySelector('.link-nav2');
-    var linkNav3 = document.querySelector('.link-nav3');
-    var linkNav4 = document.querySelector('.link-nav4');
+    var linkNav = document.querySelectorAll('.link-nav');
     
-    // si on clic sur le menu burger
-        // on ajoute les class
         burger.onclick = function(){
             ul.classList.toggle('translate');
             spancheck.classList.toggle('rotate1');
             spancheck1.classList.toggle('rotate2');
         };
-    // clic sur les  liens qui permettent de scroll + fermer la nav
-        linkNav.onclick = function(){
-            ul.classList.toggle('translate');
-            spancheck.classList.toggle('rotate1');
-            spancheck1.classList.toggle('rotate2');
-        };
-        linkNav1.onclick = function(){
-            ul.classList.toggle('translate');
-            spancheck.classList.toggle('rotate1');
-            spancheck1.classList.toggle('rotate2');
-        };
-        linkNav2.onclick = function(){
-            ul.classList.toggle('translate');
-            spancheck.classList.toggle('rotate1');
-            spancheck1.classList.toggle('rotate2');
-        };
-        linkNav3.onclick = function(){
-            ul.classList.toggle('translate');
-            spancheck.classList.toggle('rotate1');
-            spancheck1.classList.toggle('rotate2');
-        };
-        linkNav4.onclick = function(){
-            ul.classList.toggle('translate');
-            spancheck.classList.toggle('rotate1');
-            spancheck1.classList.toggle('rotate2');
-        };
+        for (let i = 0; i < linkNav.length; i++){
+            linkNav[i].onclick = function(){
+                ulTranslate.classList.toggle('translate');
+                spancheck.classList.toggle('rotate1');
+                spancheck1.classList.toggle('rotate2');
+            }
+        }
 
 // changement contenu de la balise title en fonctione de si l'utilisateur est sur la page ou non
 	if (document.documentElement.lang === "fr") { // detecter si la page est en français
